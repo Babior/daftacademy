877b804a68c61f5ef891b7c1d72cf7da7fcb199a
Lizaveta Babior <liza.babior@gmail.com>
2021-04-19 09:15:29 +0200

    Task 1.3

--- main.py
+++ main.py
@@ -1,7 +1,63 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Query
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel
+from typing import List, Optional
+
+from datetime import date, timedelta
+
+fake_users_db = {
+    "johndoe": {
+        "name": "liza",
+        "surname": "podliza",
+    },
+    "alice": {
+        "name": "roma",
+        "surname": "svelodrorma",
+
+    },
+}
+
+class User(BaseModel):
+    id: int
+    name: str
+    surname: str
+    register_date: date
+    vaccination_date: date
+
 
 app = FastAPI()
+app.counter = 1
+
 
 @app.get("/")
-def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"message": "Hello World!"}
+
+
+@app.api_route("/method/", methods=["GET", "POST", "DELETE", "PUT", "OPTIONS"])
+def return_method(request: Request) -> JSONResponse:
+    """
+    Method to return request method.
+    : param: request
+    : return: request method
+    """
+    if request.method == "POST":
+        return JSONResponse({"method": request.method}, status_code=201)
+    return JSONResponse({"method": request.method})
+
+
+@app.post("/register/{user_id}", status_code=201)
+def registration(user: User):
+    app.counter += 1
+    user.id = app.counter
+    d = len(user.name) + len(user.surname)
+    user.vaccination_date = user.register_date + timedelta(len(user.name) + len(user.surname))
+    return {"id": user.id, "name": user.name, "surname": user.surname, "register_date": user.register_date, "vaccination_date": user.vaccination_date}
+
+
+# def get_user(db, name: str):
+#     if name in db:
+#         user_dict = db[name]
+#         return User(**user_dict)
+
+
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from fastapi.testclient import TestClient
 
 from main import app
+import pytest
 
 client = TestClient(app)
 
@@ -9,3 +10,13 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+def test_request_method():
+    response = client.get("/method")
+    assert response.status_code == 200
+
+
+def test_auth():
+response = client.get("/auth")
+assert response.status_code == 201
+assert response.json() == {"message": "Hello World"}
