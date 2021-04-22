0589b4bff91afb20db66f34b74846decba6aa404
Lizaveta Babior <liza.babior@gmail.com>
2021-04-22 10:55:26 +0200

    Refactored

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-from fastapi import FastAPI, Request, Response
+import hashlib
+from datetime import date, datetime, timedelta
+from typing import Dict, Optional
+from fastapi import Depends, FastAPI, HTTPException, Request, Response, status
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
-from hashlib import sha512
-from datetime import date, timedelta
-from typing import Optional
 
 
 class UserIn(BaseModel):
@@ -20,7 +20,8 @@ class UserOut(BaseModel):
 
 
 app = FastAPI()
-app.users = []
+app.counter: int = 1
+app.users: Dict[int, UserOut] = {}
 
 
 @app.get("/")
@@ -33,7 +34,7 @@ def define_method(request: Request) -> JSONResponse:
     """
     Method to return request method.
     : param: request
-    : return: request method
+    : return: JSON response with request method
     """
     if request.method == "POST":
         return JSONResponse({"method": request.method}, status_code=201)
@@ -41,35 +42,31 @@ def define_method(request: Request) -> JSONResponse:
 
 
 @app.get("/auth")
-def authorisation(password: str, password_hash: str, response: Response):
+def authorisation(password: str = "", password_hash: str = ""):
     """
-    Method to validate hashed password
+    Method to validate hashed password.
     : param: password, password_hash
-    : return: HTTP 204 if password hashed by sha512 == password_hash
+    : return: response HTTP 204 if password hashed by sha512 == password_hash
     """
-    if (password != None) and (password_hash != None) and (password != "") and (password_hash != ""):
-        response.status_code = 401
-        return
-    hashed = sha512(password.encode()).hexdigest()
-    if hashed != password_hash:
-        response.status_code = 401
-    else:
-        response.status_code = 204
+    if password is not None and password_hash is not None and password != "" and password_hash != "":
+        return Response(status_code=401)
+    hashed = hashlib.sha512(password.encode()).hexdigest()
+    if hashed == password_hash:
+        return Response(status_code=204)
+    return Response(status_code=401)
 
 
 @app.post("/register", response_model=UserOut, status_code=201)
 def registration(user: UserIn):
     """
-    Method to add users to fake db :
-    param: user's name and surname :
+    Method to add users to fake db.
+    param: user's name and surname
     return: user object with id, name, surname, registration date and
     vaccination date(=registration date + sum of letter in name and surname) HTTP 204 if
     """
-    user_id = len(app.users) + 1
+    user_id = app.counter
     register_date = date.today()
     vaccination_date = register_date + timedelta((sum(map(str.isalpha, user.name)) + sum(map(str.isalpha, user.surname))))
-    # vaccination_date = register_date + timedelta(len(user.name) + len(user.surname))
-    print(len(user.name) + len(user.surname))
     user_out = UserOut(
         id=user_id,
         name=user.name,
@@ -77,21 +74,22 @@ def registration(user: UserIn):
         register_date=register_date,
         vaccination_date=vaccination_date,
     )
-    app.users.append(user_out)
+    app.users[app.counter] = user_out
+    app.counter += 1
     return user_out
 
 
 @app.get('/patient/{id}', status_code=404, response_model=UserOut)
-def get_patient(id: int, response: Response):
+def get_patient(user_id: int):
     """
-    Method to return users by id :
-    param: user's id :
+    Method to return users by id.
+    param: user's id
     return: user object with id, name, surname, registration date and vaccination date
     """
-    if id in app.users:
-        response.status_code = 200
-        return app.users[id - 1]
-    elif id < 1:
-        response.status_code = 400
-    else:
-        response.status_code = 404
+    if user_id < 1:
+        return Response(status_code=400, detail="Invalid patient id")
+
+    if user_id not in app.users:
+        return Response(status_code=404, detail="Patient not found")
+
+    return app.users.get(user_id)
