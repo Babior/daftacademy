7327dc73b409667d9c0f74a775f8c2f2395864e9
Lizaveta Babior <liza.babior@gmail.com>
2021-05-02 21:10:09 +0300

    Files manage

--- main.py
+++ main.py
@@ -1,106 +1,88 @@
+import base64
 import hashlib
+import random
+import secrets
 
 from datetime import date, timedelta
 from typing import Dict
 from pydantic import BaseModel
 
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request, Response, Header, HTTPException
 from fastapi.responses import JSONResponse
 from starlette.responses import HTMLResponse
 
+from typing import Dict, Optional
 
-class UserIn(BaseModel):
+from fastapi import Depends, FastAPI, Response, status, Request, HTTPException
+from fastapi.templating import Jinja2Templates
+from fastapi.security import APIKeyCookie, HTTPBasic, HTTPBasicCredentials
+from pydantic import BaseModel
+from starlette.responses import RedirectResponse
+
+
+class Patient(BaseModel):
     name: str
     surname: str
 
 
-class UserOut(BaseModel):
-    id: int
-    name: str
-    surname: str
-    register_date: date
-    vaccination_date: date
+class DaftAPI(FastAPI):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.counter: int = 0
+        self.storage: Dict[int, Patient] = {}
+        self.security = HTTPBasic(auto_error=False)
+        self.secret_key = "kluczyk"
+        self.API_KEY = "session"
+        self.cookie_sec = APIKeyCookie(name=self.API_KEY, auto_error=False)
+        self.templates = Jinja2Templates(directory="templates")
 
 
-app = FastAPI()
-app.counter: int = 1
-app.users: Dict[int, UserOut] = {}
+app = DaftAPI()
 
 
-@app.get("/")
-def root():
-    return {"message": "Hello world!"}
-
-
-@app.api_route("/method", methods=["GET", "POST", "DELETE", "PUT", "OPTIONS"])
-def define_method(request: Request) -> JSONResponse:
-    """
-    Method to return request method.
-    : param: request
-    : return: JSON response with request method
-    """
-    if request.method == "POST":
-        return JSONResponse({"method": request.method}, status_code=201)
-    return JSONResponse({"method": request.method})
-
-
-@app.get("/auth")
-def authorisation(password: str = "", password_hash: str = ""):
-    """
-    Method to validate hashed password.
-    : param: password, password_hash
-    : return: response HTTP 204 if password hashed by sha512 == password_hash
-    """
-    if password is not None and password_hash is not None and password != "" and password_hash != "":
-        return Response(status_code=401)
-    hashed = hashlib.sha512(password.encode()).hexdigest()
-    if hashed == password_hash:
-        return Response(status_code=204)
-    return Response(status_code=401)
-
-
-@app.post("/register", response_model=UserOut, status_code=201)
-def registration(user: UserIn):
-    """
-    Method to add users to fake db.
-    param: user's name and surname
-    return: user object with id, name, surname, registration date and
-    vaccination date(=registration date + sum of letter in name and surname) HTTP 204 if
-    """
-    user_id = app.counter
-    register_date = date.today()
-    vaccination_date = register_date + timedelta(
-        (sum(map(str.isalpha, user.name)) + sum(map(str.isalpha, user.surname))))
-    user_out = UserOut(
-        id=user_id,
-        name=user.name,
-        surname=user.surname,
-        register_date=register_date,
-        vaccination_date=vaccination_date,
-    )
-    app.users[app.counter] = user_out
-    app.counter += 1
-    return user_out
-
-
-@app.get('/patient/{id}', status_code=404, response_model=UserOut)
-def get_patient(user_id: int):
-    """
-    Method to return users by id.
-    param: user's id
-    return: user object with id, name, surname, registration date and vaccination date
-    """
-    if user_id < 1:
-        return Response(status_code=400, detail="Invalid patient id")
-
-    if user_id not in app.users:
-        return Response(status_code=404, detail="Patient not found")
-
-    return app.users.get(user_id)
-
-
+# Task3.1
 @app.get("/hello")
 async def hello():
     today_date = date.today().isoformat()
     content = "<h1>Hello! Today date is {}</h1>".format(today_date)
     return HTMLResponse(content=content)
+
+
+# Task3.2
+def check_password_and_generate_token(header):
+    auth_header = header
+    decoded_data = auth_header.split(" ")[1]
+    decoded_data_bytes = decoded_data.encode('ascii')
+    base_decode_result = base64.b64decode(decoded_data_bytes)
+    result = base_decode_result.decode('ascii')
+    user, password = tuple(result.split(":"))
+    if user == "4dm1n" and password == "NotSoSecurePa$$":
+        random_num = random.randint(0, 1000)
+        token = hashlib.sha512((user + password + str(random_num)).encode())
+        return token.hexdigest()
+    return False
+
+
+@app.post("/login_session")
+async def login_session(response: Response, Authorization: str = Header(None)):
+    token = check_password_and_generate_token(Authorization)
+    if token:
+        if len(app.session_token) == 3:
+            app.session_token = app.session_token[1:]
+        app.session_token.append(token)
+        response.set_cookie(key="session_token", value=token)
+        response.status_code = 201
+        return "OK"
+    raise HTTPException(status_code=401, detail='Unauthorized')
+
+
+@app.post("/login_token")
+async def login_token(response: Response, Authorization: str = Header(None)):
+    token = check_password_and_generate_token(Authorization)
+    if token:
+        if len(app.token) == 3:
+            app.token = app.token[1:]
+        app.token.append(token)
+        response.status_code = 201
+        return {"token": token}
+    raise HTTPException(status_code=401, detail='Unauthorized')
