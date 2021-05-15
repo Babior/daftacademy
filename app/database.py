ddfd862a6de4cb69184bbabdbe6722b0ff7d978b
Lizaveta Babior <liza.babior@gmail.com>
2021-05-15 15:59:33 +0300

    Task 5.0

--- app/database.py
+++ app/database.py
@@ -6,9 +6,9 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine('postgres://nwtxaahetowokv:7d1f3b103b6c9047a047ac557035affdd5fb71fe6e26c18155597c7cc05bc811@ec2-54-72-155-238.eu-west-1.compute.amazonaws.com:5432/dab061v57j8i6l')
 conn = psycopg2.connect(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=conn)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 # Dependency
