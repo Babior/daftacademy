6467da767a72c304bb5261f1bb4f262071a86549
Lizaveta Babior <liza.babior@gmail.com>
2021-05-15 16:26:38 +0300

    Task 5.0

--- app/database.py
+++ app/database.py
@@ -1,13 +1,11 @@
 import os
-
-import heroku as heroku
 import psycopg2
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DATABASE_URL = os.getenv('SQLALCHEMY_DATABASE_URL')
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
