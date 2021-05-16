89ed7e46c4ba316b1ff39cb1f2e1b1eda263361f
Lizaveta Babior <liza.babior@gmail.com>
2021-05-16 08:55:51 +0300

    Task 5.0

--- app/database.py
+++ app/database.py
@@ -4,8 +4,8 @@ import psycopg2
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv('DATABASE_URL')
-engine = create_engine(DATABASE_URL)
+SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
